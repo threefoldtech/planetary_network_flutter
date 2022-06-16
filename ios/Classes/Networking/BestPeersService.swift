98beff603870e806f50476c2a6046e60a84b437a
Lennert <lennert.defauw@jimber.org>
2022-06-16 09:38:00 +0200

    WIP

--- ios/Classes/Networking/BestPeersService.swift
+++ ios/Classes/Networking/BestPeersService.swift
@@ -25,65 +25,64 @@ public class BestPeersService : NSObject {
     var peers = [PeerInfo]()
     
     public func GetBestPeers(completionHandler: (BestPeersResult) -> Void) -> Void {
-        guard let url = URL(string: "https://jimber.io/peers.html") else {
+        guard let url = URL(string: "https://raw.githubusercontent.com/threefoldtech/planetary_network/main/threefold-nodelist") else {
             completionHandler(BestPeersResult.Error(message: "Invalid url"))
             return
         }
         
-        do {
+            do {
             let htmlData = try String(contentsOf: url, encoding: .ascii)
-        
-            let pattern = "((tcp|tls):\\/\\/(.*?))<\\/"
-            let regex = try! NSRegularExpression(pattern: pattern)
-            
-            var fastPeers = 0
-            
-            regex.enumerateMatches(in: htmlData, options: [], range: NSRange(location: 0, length: htmlData.utf16.count)) { (result, _, stop) in
-                guard let group = result?.range(at: 1), let range = Range(group, in: htmlData) else {
-                    return
-                }
-            
-                let fullAddress = String(htmlData[range])
-                let scheme = fullAddress.contains("tls://") ? Scheme.tls : Scheme.tcp
-                var split = fullAddress.components(separatedBy: ":")
-                let port = Int32(split.popLast()!)!
-                let address = split.joined(separator: ":")
-                    .replacingOccurrences(of: "tls://", with: "")
-                    .replacingOccurrences(of: "tcp://", with: "")
-                                
-                NSLog("Ping \(fullAddress)")
                 
-                let startTime = DispatchTime.now()
-                let client = TCPClient(address: address, port: port)
-                switch client.connect(timeout: 1) {
-                    case .success:
-                        let endTime = DispatchTime.now()
-                        let ping = self.CalculateMillisecondsBetween(startTime, endTime)
-                        
-                        let peer = PeerInfo(scheme: scheme, address: client.address, port: UInt16(client.port), ping: ping)
-                        
-                        peers.append(peer)
-                        
-                        if (ping < 75) {
-                            NSLog("Found fast host \(address)")
-                            fastPeers += 1
-                        }
-                    case .failure:
-                        NSLog("Ping failed for host \(address)")
+                guard let data = htmlData.data(using:String.Encoding.utf8),
+                      let arrayOfStrings = try JSONSerialization.jsonObject(with: data, options: []) as? [String] else {
+                        fatalError()
                 }
                 
-                if (fastPeers > 2) {
-                    NSLog("Found 3 fast hosts (<75ms)");
-                    stop.pointee = true
+                
+                var fastPeers = 0
+                
+                for address in arrayOfStrings {
+                    let fullAddress = address;
+                    let scheme = fullAddress.contains("tls://") ? Scheme.tls : Scheme.tcp
+                    var split = fullAddress.components(separatedBy: ":")
+                    let port = Int32(split.popLast()!)!
+                    let address = split.joined(separator: ":")
+                        .replacingOccurrences(of: "tls://", with: "")
+                        .replacingOccurrences(of: "tcp://", with: "")
+                    
+                    let startTime = DispatchTime.now()
+                    let client = TCPClient(address: address, port: port)
+                    switch client.connect(timeout: 1) {
+                        case .success:
+                            let endTime = DispatchTime.now()
+                            let ping = self.CalculateMillisecondsBetween(startTime, endTime)
+                            
+                            let peer = PeerInfo(scheme: scheme, address: client.address, port: UInt16(client.port), ping: ping)
+                            
+                            peers.append(peer)
+                            
+                            if (ping < 75) {
+                                NSLog("Found fast host \(address)")
+                                fastPeers += 1
+                            }
+                        case .failure:
+                            NSLog("Ping failed for host \(address)")
+                    }
+                    
+                    if (fastPeers > 2) {
+                        NSLog("Found 3 fast hosts (<75ms)");
+                        break;
+                    }
+                    
                 }
-            }
-            
+
             peers.sort {
                 $0.ping < $1.ping
             }
-            
+                
             completionHandler(BestPeersResult.Success(peers: peers))
         } catch {
+            NSLog("Error in searching best peers...")
             completionHandler(BestPeersResult.Error(message: error.localizedDescription))
         }
     }
